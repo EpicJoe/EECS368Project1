dab20ed112490f9667db890ef8637eb8e6f2bd66
Wenxuan Hu <wenxuanhu66@gmail.com>
2020-10-31 23:08:25 -0500

    finished the chess board

--- public/game.js
+++ public/game.js
@@ -1,3 +1,4 @@
+//button
 function startGame(){
     mode = "startgame"
     window.location.href = "game.html"
@@ -5,4 +6,28 @@ function startGame(){
 
 function startGameRule(){
     window.location.href = "gamerule.html"
+}
+
+
+//chessboard
+var chess = document.getElementById('chess');
+var context = chess.getContext('2d');
+
+
+var img= new Image();
+img.src="img/chessbackground.jpg";
+img.onload=function(){
+    context.drawImage(img,0,0,450,450);
+    drawChessboard();
+}
+
+function drawChessboard(){
+    for(var i=0; i<15; i++){
+        context.moveTo(15, 15 + i * 30);
+        context.lineTo(435, 15 + i * 30);
+        context.stroke();
+        context.moveTo(15 + i * 30, 15);
+        context.lineTo(15 + i * 30, 435);
+        context.stroke();
+    }
 }
