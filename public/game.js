93ca107d626d38998dbb7fab5ad06fe1d984fa27
Wenxuan Hu <wenxuanhu66@gmail.com>
2020-11-01 00:39:30 -0500

    click to set have problem, need to fix

--- public/game.js
+++ public/game.js
@@ -13,7 +13,6 @@ function startGameRule(){
 var chess = document.getElementById('chess');
 var context = chess.getContext('2d');
 
-
 var img= new Image();
 img.src="img/chessbackground.jpg";
 img.onload=function(){
@@ -33,21 +32,33 @@ function drawChessboard(){
 }
 
 //chess style on the board
-function chess(i,j,chesscolor){
+
+var chess = function(i,j,chesscolor){
     context.beginPath();
     context.arc(15+i*30, 15+j*30,0,2*Math.PI);
     context.closePath();
-    var gradient=context.radialGradient(15+i*30+2,15+j*30-2,15,15+i*30,15+j*30,0);
+    var gradient=context.createRadialGradient(15+i*30+2,15+j*30-2,15,15+i*30,15+j*30,0);
     if(chesscolor){
-        gradient.addColor(0, "#0a0a0a");
-        gradient.addColor(1, "#636766");
+        gradient.addColorStop(0, "#0a0a0a");
+        gradient.addColorStop(1, "#636766");
     }
     else{
-        gradient.addColor(0, "#D1D1D1");
-        gtadient.addColor(1, "#F9F9F9");
+        gradient.addColorStop(0, "#D1D1D1");
+        gtadient.addColorStop(1, "#F9F9F9");
     }
     context.fillStyle=gradient;
     context.fill();
 }
 
 //click to set chess
+var chesscolor=true;
+var click = document.getElementById('click');
+var context = click.getContext('2d');
+click.onclick=function(getCoordinate){
+    var x=getCoordinate.offsetX;
+    var y=getCoordinate.offsetY;
+    var i=Math.floor(x/30);
+    var j=Math.floor(y/30);
+    chess(i,j,chesscolor);
+    chesscolor=!chesscolor;
+}
